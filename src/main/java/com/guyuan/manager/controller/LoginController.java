615dc3f2375a559d5d99aa67fbbc0ff6341c0e58
Aseubel <1228392319@qq.com>
2024-12-19 10:42:45 +0800

    将login事件抽成单独方法

--- src/main/java/com/guyuan/manager/controller/LoginController.java
+++ src/main/java/com/guyuan/manager/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.guyuan.manager.controller;
 
 import cn.hutool.core.util.ObjectUtil;
 import com.guyuan.manager.Entity.UserEntity;
+import com.guyuan.manager.annotation.DoRequest;
 import com.guyuan.manager.service.IUserService;
 import com.guyuan.manager.util.HostHolder;
 import com.guyuan.manager.util.TransferScene;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -31,9 +33,6 @@ public class LoginController implements Initializable {
     @FXML
     private PasswordField passwordField;
 
-    @FXML
-    private Button loginButton;
-
     @Autowired
     private IUserService userService;
 
@@ -45,55 +44,49 @@ public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loginButton.setOnAction(actionEvent -> {
-//            try {
-//                transferScene.transfer((Stage)usernameField.getScene().getWindow(),"index");
-//                String userName = usernameField.getText();
-//                if(userName.isEmpty()) return;
-//            } catch (Exception e) {
-//                throw new RuntimeException(e);
-//            }
 
-            String userName = usernameField.getText();
-            String password = passwordField.getText();
-            if (userName.isEmpty() || password.isEmpty()) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("错误提示");
-                alert.setHeaderText("输入无效！用户名和密码不能为空");
-                alert.setContentText("请重新输入");
-                alert.showAndWait();
-                return;
+    }
+
+    public void login(ActionEvent actionEvent) {
+        String userName = usernameField.getText();
+        String password = passwordField.getText();
+        if (userName.isEmpty() || password.isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("错误提示");
+            alert.setHeaderText("输入无效！用户名和密码不能为空");
+            alert.setContentText("请重新输入");
+            alert.showAndWait();
+            return;
+        }
+        UserEntity user = userService.getUserByName(userName);
+        if (ObjectUtil.isEmpty(user)) {
+            log.error("用户不存在");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("错误提示");
+            alert.setHeaderText("用户不存在！");
+            alert.setContentText("请重新输入或联系管理员添加账号");
+            alert.showAndWait();
+            return;
+        }
+        if (!ObjectUtil.isEmpty(user) && user.getPassword().equals(password)) {
+            try {
+                // 登录成功，设置用户信息到HostHolder中
+                HostHolder.setUsers(user);
+                // 初始化历史记录
+                TransferScene.init();
+                transferScene.transfer((Stage)usernameField.getScene().getWindow(),"index");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-            UserEntity user = userService.getUserByName(userName);
-            if (ObjectUtil.isEmpty(user)) {
-                log.error("用户不存在");
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("错误提示");
-                alert.setHeaderText("用户不存在！");
-                alert.setContentText("请重新输入或联系管理员添加账号");
-                alert.showAndWait();
-                return;
-            }
-            if (!ObjectUtil.isEmpty(user) && user.getPassword().equals(password)) {
-                try {
-                    // 登录成功，设置用户信息到HostHolder中
-                    HostHolder.setUsers(user);
-                    // 初始化历史记录
-                    TransferScene.init();
-                    transferScene.transfer((Stage)usernameField.getScene().getWindow(),"index");
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            else {
-                // 弹出错误提示框
-                log.error("用户名或密码错误");
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("错误提示");
-                alert.setHeaderText("登录失败");
-                alert.setContentText("用户名或密码错误");
-                alert.showAndWait();
-            }
-        });
+        }
+        else {
+            // 弹出错误提示框
+            log.error("用户名或密码错误");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("错误提示");
+            alert.setHeaderText("登录失败");
+            alert.setContentText("用户名或密码错误");
+            alert.showAndWait();
+        }
     }
 }
